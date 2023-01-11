4a84c045d937a74b827b20e752529c9547248fa6
maxbond <ceo@rf.ru>
2023-01-11 11:47:19 +0300

    bug fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Abstract - Creative Agency Personal Portfolio Template',
+    title: 'WebDev',
     description:
-      'Abstract is a beautiful template built using React and Gatsby for creative agency or personal projects',
+      'WebDev',
   },
   pathPrefix: '/',
   plugins: [
